2431b044e6008d1abbd0938ce8d2306024d0fc40
petfoodmatch <bigfunpartyshop@gmail.com>
2024-11-28 21:22:07 -0800

    Delete istressed-plugin.js

--- istressed-plugin.js
+++
@@ -1,7 +0,0 @@
-{
-  "name": "Distressed Filter Plugin",
-  "description": "Applies a distressed filter to designs for a vintage look.",
-  "version": "1.0",
-  "entry": "https://petfoodmatch.github.io/canva-plugin/distressed-plugin.js",
-  "permissions": ["canva:edit"]
-}
